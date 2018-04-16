46e5225524377bda679f6a39f6e18e5101d21baa
Yerbol Panaberdiyev <panaberdiev@gmail.com>
2018-04-16 14:18:41 -0500

    Threads lab done

--- src/lab_threads_1/Main.java
+++ src/lab_threads_1/Main.java
@@ -1,31 +1,49 @@
 package lab_threads_1;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Main {
+	static ArrayList<Integer> arrPrime = new ArrayList<Integer>();
 	public static void main(String[] args) {
 		//System.out.println(MainThreadPrime.isPrime(15));
 		int processNum =0;
 		int theadsNumber = 5;
 		int limit = 1000000;
 		int step = limit/theadsNumber;
+		ArrayList<Thread> tArray = new  ArrayList<Thread>();
 		
-		//Lab 1 level
+		
+		// Lab 1 level // uncomment if you want to check, also comment below code
 		/*for(int t = 0; t<theadsNumber;t++) {
 			MainThreadPrime p = new MainThreadPrime(processNum++, t*step+1, step);
 			new Thread(p).start();
 		}*/
 		
+		// Lab 2 level
 		processNum =0;
 		theadsNumber = 10;
-		limit = 1000000;
 		step = limit/theadsNumber;
 		
-		// Lab 2 level
+		
 		for(int t = 0; t<theadsNumber;t++) {
 			MainThreadPrime p = new MainThreadPrime(processNum++, t*step+1, step);
-			new Thread(p).start();
+			tArray.add(new Thread(p));
+			tArray.get(tArray.size()-1).start(); //start
 		}
-				
+		
+		
+		tArray.forEach(t->{
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		});
+		
+		Collections.sort(arrPrime);
+		System.out.println(arrPrime);		
 		
 	}
 
--- src/lab_threads_1/MainThreadPrime.java
+++ src/lab_threads_1/MainThreadPrime.java
@@ -1,13 +1,10 @@
 package lab_threads_1;
 
-import java.util.ArrayList;
-
 public class MainThreadPrime implements Runnable {
-	
+
 	int threadNumber = 0;
 	int start, range;
-	static ArrayList<Integer> arrPrime = new ArrayList<Integer>();
-	
+
 	public MainThreadPrime(int x, int start, int range) {
 		super();
 		this.threadNumber = x;
@@ -17,27 +14,25 @@ public class MainThreadPrime implements Runnable {
 
 	public void run() {
 		System.out.println("Start thread:" + threadNumber);
-		
-		
-		for(int i = start; i<(start + range); i++) {
-			//System.out.println("Thread: " + threadNumber + "["+ i + "] " + (isPrime(i)?" prime":" not Prime"));
-			if(isPrime(i)) {
-				System.out.println("Thread: " + threadNumber + "["+ i + "]");
-				synchronized (MainThreadPrime.arrPrime){ // synchronized keyword on block of  code
-					MainThreadPrime.arrPrime.add(i);
+
+		for (int i = start; i < (start + range); i++) {
+			// System.out.println("Thread: " + threadNumber + "["+ i + "] " +
+			// (isPrime(i)?" prime":" not Prime"));
+			if (isPrime(i)) {
+				System.out.println("Thread: " + threadNumber + "[" + i + "]");
+				synchronized (Main.arrPrime) { // synchronized keyword on block  of code
+					Main.arrPrime.add(i);
 				}
 			}
 		}
-		
-		System.out.println(arrPrime);
-		
+
 	}
 
 	public static boolean isPrime(int n) {
 		for (int i = 2; i < n; i++)
 			if ((n % i) == 0)
 				return false;
-		
+
 		return true;
 	}
 
