ca61bb65c7aacefad8dcee88ff52e84691702c32
Yerbol Panaberdiyev <panaberdiev@gmail.com>
2018-04-20 17:40:39 -0500

    Lab 8, #TODO last level 3

--- src/lab_8/ProcessingEmployees.java
+++ src/lab_8/ProcessingEmployees.java
@@ -1,15 +1,20 @@
 package lab_8;
 
+import java.util.ArrayList;
 // Program 4: ProcessingEmployees.java
 // Processing streams of Employee objects.
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ProcessingEmployees {
 	public static void main(String[] args) {
@@ -21,7 +26,67 @@ public class ProcessingEmployees {
 
 		// get List view of the Employees
 		List<Employee> list = Arrays.asList(employees);
+		
+		// 1)  Count the number of last names that begin with the letter  ‘B’.  Print out this number.
+		long c = list.stream().filter(e->e.getLastName().startsWith("B")).count();
+		System.out.println(String.format("The number of last names that begin with the letter  ‘B’: %s",  c));
+		
+		// 2) Print out all of the Employee objects whose last name begins with the letter  ‘B’  in sorted order. 
+		Util.sop("Print out all of the Employee objects whose last name begins with the letter  ‘B’  in sorted order. ");
+		Function<Employee, String> byLN = Employee::getLastName;
+		list.stream()
+		.sorted(Comparator.comparing(byLN))
+		.filter(e->e.getLastName().startsWith("B"))
+		.forEach(e->Util.sop(e.toString()));
+		
+		// 3)  Print out all of the Employee objects whose last name begins with the letter  ‘B’  and change their first name and last name to be All capital letters.
+		Util.sop("Print out all of the Employee objects whose last name begins with the letter  ‘B’  and change their first name and last name to be All capital letters.");
+		list.stream().forEach(e->{
+			if(e.getLastName().startsWith("B")) {
+					//e.setFirstName(e.getFirstName().toUpperCase()); // uncomment to check
+					//e.setLastName(e.getLastName().toUpperCase());
+			}
+		});
+		list.stream().forEach(e->Util.sop(e.toString()));
+		
+		// Print out All of the employee objects, but if the last name begins with the letter  ‘B’,  then capitalize all the letters in the last name.  
+		Util.sop("Print out all of the Employee objects whose last name begins with the letter  ‘B’  and change their first name and last name to be All capital letters.");
+		list.stream().forEach(e->{
+			if(e.getLastName().startsWith("B")) 
+					 e.setLastName(e.getLastName().toUpperCase()); // uncomment to check
+		});
+		list.stream().forEach(e->Util.sop(e.toString()));
+		
+		
+		Util.sop("4.1)  Use the  Collectors.joining  method to print out All Employee objects");
+		Util.sop(list.stream().map(Employee::toString).collect(Collectors.joining(", ")));
 
+		Util.sop("4.2)  Use the  Collectors.joining  method to print out All Employee objects, and separate each one with a delimeter of  “---\n---“ ");
+		Util.sop(list.stream().map(Employee::toString).collect(Collectors.joining("---\n---")));
+		
+		Util.sop("Print out all of the Employee objects’ last names, whose last name begins with the letter  ‘I’  in sorted order, and get rid of all the duplicates.  Print out only the last names.");
+		list.stream().filter(e->e.getLastName().startsWith("I")).sorted(Comparator.comparing(Employee::getLastName)).map(Employee::getLastName).distinct().forEach(e->Util.sop(e));  
+		
+		Util.sop("Print out the average of all the salaries.");
+		Util.sop(String.format("First method %.2f", list.stream().collect(Collectors.summarizingDouble(Employee::getSalary)).getAverage()));
+		
+		Util.sop(String.format("Second method %.2f", list.stream().map(e->e.getSalary()).collect(Collectors.summarizingDouble(Double::valueOf)).getAverage()));
+		
+		Util.sop("7)  Use the  ‘reduce’  method to print out the total salary of all employees.");
+		Util.sop(String.format("Total salary: %.2f",list.stream().map(x->x.getSalary()).reduce(.0, (x, y) -> (x + y))));
+		
+		Util.sop("8)  Print out only the first names of all the employees.  Use the  ‘map’  method to accomplish this.");
+		list.stream().map(x->x.getFirstName()).forEach(x->Util.sop(x));
+		
+		Util.sop("9)  Create an infinite stream of even numbers (0, 2, 4, …) and then, eventually print out only the first 20 even numbers from this stream.");
+        Stream.iterate(0, i-> i+2).limit(20).forEach(x->Util.sop(x.toString()));
+        
+        Util.sop("9-1)  Create an infinite stream of even numbers (0, 2, 4, …) and then, eventually print out only the first 20 even numbers from this stream.");
+        Stream.iterate(0, i-> i+1).filter(x->x%2==0).limit(20).forEach(x->Util.sop(x.toString()));
+        
+		System.exit(0);
+		
+		
 		// display all Employees
 		System.out.println("Complete Employee list:");
 		list.stream().forEach(System.out::println); // A method reference.
@@ -88,7 +153,7 @@ public class ProcessingEmployees {
 		 * 
 		 * HR 4 IT 8 Sales 12
 		 */
-
+/*
 		// sum of Employee salaries with DoubleStream sum method
 		System.out.printf("%nSum of Employees' salaries (via sum method): %.2f%n",
 				list.stream().mapToDouble(Employee::getSalary).sum());
@@ -99,6 +164,7 @@ public class ProcessingEmployees {
 
 		// average of Employee salaries with DoubleStream average method
 		System.out.printf("Average of Employees' salaries: %.2f%n",
-				list.stream().mapToDouble(Employee::getSalary).average().getAsDouble());
+				list.stream().mapToDouble(Employee::getSalary).average().getAsDouble());*/
 	} // end main
+
 } // end class ProcessingEmployees
---
+++ src/lab_8/Util.java
@@ -0,0 +1,7 @@
+package lab_8;
+
+public class Util {
+	public static void sop(String s){
+		System.out.println(s);
+	}
+}
